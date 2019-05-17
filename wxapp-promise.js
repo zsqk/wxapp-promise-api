3f1a41a0167d28d9e19c25cc52daeac2a59f0dba
iugo <iugogogo@gmail.com>
2019-05-17 18:26:34 +0800

    opt 新增 消息提示; opt 新增同步获取系统信息

--- wxapp-promise.js
+++ wxapp-promise.js
@@ -1,13 +1,36 @@
 /* global wx */
 
 export {
+  alert,
   request,
   setStorage,
   getSystemInfo,
+  getSystemInfoSync,
 };
 
 /**
- * 网络请求
+ * 显示消息提示框 wx.showToast
+ * https://developers.weixin.qq.com/miniprogram/dev/api/wx.showToast.html
+ * @param {string} title
+ * @param {Object} [options]
+ * @param {string} [options.icon=success] 枚举: success, loading, none
+ * @param {string} [options.image] 自定义图标, 优先级高于 icon
+ * @param {number} [options.duration=1500] 提示的延迟时间 毫秒
+ * @param {boolean} [options.mask=false] 是否显示透明蒙层，防止触摸穿透
+ * @return {Promise<void>}
+ */
+function alert(title, options = {}) {
+  return new Promise((resolve, reject) => {
+    wx.showToast(Object.assign({
+      title,
+      success: resolve,
+      fail: reject,
+    }, options));
+  });
+}
+
+/**
+ * 网络请求 wx.request
  * @param {String} url
  * @param {Object} [options]
  * @param {Object} options.header
@@ -29,7 +52,7 @@ function request(url, options = {}) {
 }
 
 /**
- * 本地缓存
+ * 本地缓存 wx.setStorage
  * @param {String} key - 本地缓存中的指定的 key
  * @param {Object} data - 需要存储的内容
  * @param {Object} options - 可选配置项
@@ -51,7 +74,7 @@ function setStorage(key, data, options = {}) {
 }
 
 /**
- * 获取系统信息
+ * 获取系统信息 wx.getSystemInfo
  * 建议使用 wx.getSystemInfoSync() 替代
  * @return {Promise<SystemInfo>}
  * @link https://developers.weixin.qq.com/miniprogram/dev/api/wx.getSystemInfo.html
@@ -65,6 +88,14 @@ function getSystemInfo() {
       fail: reject,
     });
   });
+}
+
+/**
+ * 同步获取系统信息 wx.getSystemInfoSync
+ * @return {SystemInfo}
+ */
+function getSystemInfoSync() {
+  return wx.getSystemInfoSync();
 }
 
 /**
